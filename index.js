15a1e8a52a1da25640d24b9754d569e98169c7f3
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-02-14 00:24:46 -0300

    v9

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const puppeteer = require('puppeteer');
 
 let browser;
 
+function removeFollowers(str) {
+  return str.replace(/\s*K\s*followers\s*/, '');
+}
+
 app.get('/', async (req, res) => {
   try {
     if (!browser) {
@@ -16,16 +20,41 @@ app.get('/', async (req, res) => {
     const page = await browser.newPage();
     await page.setDefaultNavigationTimeout(0);
 
+
+    // Tiktok
     await page.goto('https://www.tiktok.com/@birobirobirodev', {
       waitUntil: 'load',
       setTimeout: ms('0s'),
     });
     await page.waitForSelector('[data-e2e="followers-count"]');
-    const followersCount = await page.$eval('[data-e2e="followers-count"]', (el) => el.innerText);
+    const TiktokFollowers = await page.$eval('[data-e2e="followers-count"]', (el) => el.innerText);
 
+    // KooApp
+    await page.goto('https://www.kooapp.com/profile/birobirobiro', {
+      waitUntil: 'load',
+      setTimeout: ms('0s'),
+    });
+
+    await page.waitForTimeout('2ms');
+
+    await page.waitForSelector('span[class^="Follow_follow__count__t1dQ2"');
+    const KooAppFollowers = await page.$eval('span[class^="Follow_follow__count__t1dQ2"', (el) => el.innerText);
+
+    // LinkedIn
+    await page.goto('https://www.linkedin.com/in/birobirobiro/?original_referer=', {
+      waitUntil: 'load',
+      setTimeout: ms('0s'),
+    });
+
+    await page.waitForTimeout('2ms');
+
+    await page.waitForSelector('span[class^="top-card__subline-item"');
+    const LinkedInFollowers = await page.$eval('span[class^="top-card__subline-item"', (el) => el.innerText);
 
     res.send({
-      "followers": followersCount,
+      "tiktok": TiktokFollowers,
+      "kooapp": KooAppFollowers,
+      "linkedin": Number(removeFollowers(LinkedInFollowers)) * 1000,
     });
   } catch (err) {
     console.error(err);
